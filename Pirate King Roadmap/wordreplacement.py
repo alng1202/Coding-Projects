c4ec3511d7d8d1952792e7e06b01a18b1b64b972
Alan Nguyen <alanhoangng@gmail.com>
2024-04-20 02:29:40 -0700

    wat

---
+++ Pirate King Roadmap/wordreplacement.py
@@ -0,0 +1,11 @@
+# 4/20/224
+
+def word_replacement():
+    str = "Hello! My name is Alan."
+    word_to_replace = input("Enter word to replace: ")
+    word = input("Enter word to replace with: ")
+    print(str.replace(word_to_replace, word))
+
+    word_replacement
+
+word_replacement()
